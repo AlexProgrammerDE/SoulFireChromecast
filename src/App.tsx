7b6b80e406912abedc52e8ea2ed493810c4e8d80
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-06 10:36:21 +0200

    Cleanup some logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,15 @@ import {useEffect, useState} from "react";
 import {Loader2Icon} from "lucide-react";
 
 export default function App() {
-  const [loading, setLoading] = useState(true)
   const [logs, setLogs] = useState<string[]>([])
 
   useEffect(() => {
     const namespace = 'urn:x-cast:com.soulfiremc'
     const listener: SystemEventHandler = (customEvent) => {
-      // Set to not loading if it is loading
-      setLoading(current => {
-        if (current) {
-          return false
-        }
-
-        return current
-      })
-
       setLogs((prevLogs) => [...prevLogs, JSON.stringify(customEvent, null, 2)])
     }
     cast.framework.CastReceiverContext.getInstance().addCustomMessageListener(namespace, listener)
     cast.framework.CastReceiverContext.getInstance().start({
-      customNamespaces: {
-        "urn:x-cast:com.soulfiremc": cast.framework.system.MessageType.JSON
-      },
       skipPlayersLoad: true,
     })
 
@@ -35,30 +22,28 @@ export default function App() {
   }, []);
 
   return (
-      <>
-        <div className="h-screen w-screen flex">
-          {
-            loading ? (
-                <div className="m-auto flex flex-col gap-4">
-                  <h1 className="text-2xl">SoulFire is loading...</h1>
-                  <div className="mx-auto">
-                    <Loader2Icon className="animate-spin" size={32}/>
-                  </div>
+      <div className="h-screen w-screen flex">
+        {
+          logs.length === 0 ? (
+              <div className="m-auto flex flex-col gap-4">
+                <h1 className="text-2xl">SoulFire is loading...</h1>
+                <div className="mx-auto">
+                  <Loader2Icon className="animate-spin" size={32}/>
                 </div>
-            ) : (
-                <div className="m-auto flex flex-col gap-4">
-                  <h1 className="text-3xl">SoulFire is ready!</h1>
-                  <div className="overflow-auto h-96 w-96 border border-gray-300 rounded-lg">
-                    <ul className="p-4">
-                      {logs.map((log, index) => (
-                          <li key={index} className="p-2 border-b border-gray-300">{log}</li>
-                      ))}
-                    </ul>
-                  </div>
+              </div>
+          ) : (
+              <div className="m-auto flex flex-col gap-4">
+                <h1 className="text-3xl">SoulFire is ready!</h1>
+                <div className="overflow-auto h-96 w-96 border border-gray-300 rounded-lg">
+                  <ul className="p-4">
+                    {logs.map((log, index) => (
+                        <li key={index} className="p-2 border-b border-gray-300">{log}</li>
+                    ))}
+                  </ul>
                 </div>
-            )
-          }
-        </div>
-      </>
+              </div>
+          )
+        }
+      </div>
   )
 }
