16d232fee8a28f555c4d7bea4f698d860ff3bb12
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-06 08:02:07 +0200

    Start cast receiver context

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+cast.framework.CastReceiverContext.getInstance().start();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
