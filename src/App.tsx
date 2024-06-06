ac1a6d59428e1efdabbbf3dac8157add5fc00c66
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-06 08:42:38 +0200

    Add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import 'non.geist'
+import {useState} from "react";
+import {Loader2Icon} from "lucide-react";
+
 export default function App() {
+  const [loading, setLoading] = useState(true)
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="h-screen w-screen flex">
+        {
+          loading ? (
+            <div className="m-auto flex flex-col gap-8">
+              <h1 className="text-3xl">SoulFire is loading...</h1>
+              <div className="mx-auto">
+                <Loader2Icon className="animate-spin" size={48} />
+              </div>
+            </div>
+          ) : (
+            <div className="m-auto">
+              <div className="text-3xl">Hello World!</div>
+            </div>
+          )
+        }
+      </div>
     </>
   )
 }
