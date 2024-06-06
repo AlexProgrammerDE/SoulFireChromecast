3188926d2b73bfd87703db4d94f30f60c3b174a0
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-06 10:04:18 +0200

    Improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -32,14 +32,14 @@ export default function App() {
         <div className="h-screen w-screen flex">
           {
             loading ? (
-                <div className="m-auto flex flex-col gap-8">
+                <div className="m-auto flex flex-col gap-4">
                   <h1 className="text-2xl">SoulFire is loading...</h1>
                   <div className="mx-auto">
                     <Loader2Icon className="animate-spin" size={32}/>
                   </div>
                 </div>
             ) : (
-                <div className="m-auto">
+                <div className="m-auto flex flex-col gap-4">
                   <h1 className="text-3xl">SoulFire is ready!</h1>
                   <div className="overflow-auto h-96 w-96 border border-gray-300 rounded-lg">
                     <ul className="p-4">
