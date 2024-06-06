b8a8ec56442d401267a552c575060de3211ef15c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-06 08:58:17 +0200

    Call start _after_ render

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-cast.framework.CastReceiverContext.getInstance().start();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+cast.framework.CastReceiverContext.getInstance().start();
