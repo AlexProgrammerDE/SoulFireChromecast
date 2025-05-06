1b9dc7761606177970ba5e1c4f170787a33ac31a
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-05-06 17:16:24 +0200

    feat: update to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import tailwind from "@tailwindcss/vite"
+import {defineConfig} from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwind()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
